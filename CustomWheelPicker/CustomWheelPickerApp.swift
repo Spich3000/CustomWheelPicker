952e57d53c8c53b5d12da41d54f46e423adba424
Spich <spichakovda@gmail.com>
2023-07-01 05:17:02 +0300

    Initial Commit

---
+++ CustomWheelPicker/CustomWheelPickerApp.swift
@@ -0,0 +1,17 @@
+//
+//  CustomWheelPickerApp.swift
+//  CustomWheelPicker
+//
+//  Created by Дмитрий Спичаков on 01.07.2023.
+//
+
+import SwiftUI
+
+@main
+struct CustomWheelPickerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
